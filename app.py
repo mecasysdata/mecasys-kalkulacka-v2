0ab8a6a07cd28cfd5de34a653562d68f7f4eefd1
mecasysdata <mecasysdata@gmail.com>
2026-03-23 09:12:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import re 
 from datetime import date
 
 # 1. premenná - dátum
@@ -24,9 +23,10 @@ d = st.number_input("Priemer komponentu [mm]", min_value=0.0, step=0.1, format="
 # 7. premenná - l
 l = st.number_input("Dĺžka komponentu [mm]", min_value=0.0, step=0.1, format="%.2f")
 
-# Načítanie dát z Google Sheets pre premenné 8 a 9
+# UPRAVENÉ NAČÍTANIE: Pridávame decimal a thousands, aby Pandas hneď spravil z hustoty číslo
 sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRfPBZ4TCpQyiqybU0ADu3AMwHCi2qOKifQAOnnTWnorVNJ1SVxtN6zJzXthOxCVwtXWp__Bp_-nto0/pub?gid=1281008948&single=true&output=csv"
-df_materialy = pd.read_csv(sheet_url)
+df_materialy = pd.read_csv(sheet_url, decimal=',', thousands='\xa0') 
+# Poznámka: \xa0 je kód pre nezlomiteľnú medzeru, ktorú Google Sheets používa
 
 # 8. premenná - material
 seznam_materialov = df_materialy['material'].unique()
@@ -37,26 +37,11 @@ seznam_akosti = df_materialy[df_materialy['material'] == material]['akost'].uniq
 akost = st.selectbox("Akosť", options=seznam_akosti)
 
 # 10. premenná - hustota
-import streamlit as st
-import pandas as pd
-from datetime import date
-import re  # Pridaný import pre vyčistenie textu
-
-# ... (tvoje premenné 1 až 9 zostávajú rovnaké) ...
-
-# 10. premenná - hustota
+(ZJEDNODUŠENÁ LOGIKA)
 if material == "PLAST":
-    # Vytiahne hodnotu 'hustota' zo sheetu
     hustota_val = df_materialy[(df_materialy['material'] == "PLAST") & (df_materialy['akost'] == akost)]['hustota'].values[0]
-    
-    # Prevod na string a nahradenie čiarky bodkou
-    hustota_temp = str(hustota_val).replace(',', '.')
-    
-    # Ponecháme IBA číslice a bodku, všetko ostatné (medzery, znaky) zmizne
-    hustota_clean = re.sub(r'[^0-9.]', '', hustota_temp)
-    
-    hustota = float(hustota_clean)
-    
+    # Keďže sme to nastavili pri pd.read_csv, hustota by už mala byť číslo (float)
+    hustota = float(hustota_val)
 elif material == "NEREZ":
     hustota = 8000.0
 elif material == "OCEĽ":
@@ -72,6 +57,3 @@ elif material == "FAREBNÉ KOVY":
         hustota = 0.0
 else:
     hustota = 0.0
-
-st.write(f"Vypočítaná hustota: {hustota}")
-
